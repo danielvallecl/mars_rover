ae10ad715f5d6fd21321e26c096db8a2aad8b1b8
Daniel Valle <daniel.vallecl@gmail.com>
2018-10-23 00:59:39 -0400

    Cleanup old version and added spec_mars_rover.rb

---
+++ README.md
@@ -0,0 +1,14 @@
+
+<h1>NASA Mars Rover Challenge</h1>
+
+<h2>To run the app from the command line navigate to the directory and type:</h2>
+
+`ruby mars_rover.rb "mars_input.txt"`
+
+<h2>This will run the app and load an external test file with directions.
+
+<h2>To run the Rspec tests navigate to the directory and run: </h2>
+
+`rspec spec/spec_mars_rover.rb`
+
+<h5>Developed by Daniel Valle in Toronto on October 22nd, 2018.</h5>
--- spec/spec_mars_rover.rb
+++ spec/spec_mars_rover.rb
@@ -1,1 +1,28 @@
 
+require "spec_helper"
+require_relative "mars_rover"
+require relative "mars_class"
+
+describe MarsRover do
+
+  it 'Check if there is a Rover' do
+    rover = MarsRover.new
+  end
+
+  it 'A direction other than North, East, South or West' do
+    rover = Mars.new
+  end
+
+  it 'A turning command other than Left and Right' do
+    rover = Mars.new
+  end
+
+  it 'A Missing Parameter' do
+    rover = Mars.new
+  end
+
+  it 'A position over the limits of the plateau' do
+    rover = Mars.new
+  end
+
+end
