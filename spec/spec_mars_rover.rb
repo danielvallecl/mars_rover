d497ab3085d2a8fc7f1f4c5720ca7eddb926f6d1
Daniel Valle <daniel.vallecl@gmail.com>
2018-10-23 01:59:41 -0400

    Added more tests

--- spec/spec_mars_rover.rb
+++ spec/spec_mars_rover.rb
@@ -5,26 +5,37 @@ require_relative "../class_mars"
 
 describe "Mars Rover Functions" do
 
+  rover = MarsRover.new
+  input_data = rover.read_input_file("mars_input.txt")
+
   it 'Checks for the presence of an Input file' do
-    rover = MarsRover.new
-    expect(rover.read_input_file("mars_input.txt").size).to eq(5)
+    expect(input_data.size).to eq(5)
   end
 
   it 'Checks if there is a Plateau X and Y Information' do
-    rover = MarsRover.new
-    expect(rover.read_input_file("mars_input.txt")[0]).to eq("5 5\n")
+    expect(input_data[0]).to eq("5 5\n")
   end
 
-  
+  it 'Checks if there are Rovers set in the Input File' do
+    expect((input_data.size - 1) / 2).to eq(2)
+  end
 
-  # it 'A direction other than North, East, South or West' do
-  #   rover = Mars.new
-  # end
-  #
-  # it 'A turning command other than Left and Right' do
-  #   rover = Mars.new
-  # end
-  #
+  it 'Checks if the X, Y position are Integers' do
+    expect(input_data[1][0].to_i).to be_an_instance_of(Integer)
+    expect(input_data[1][2].to_i).to be_an_instance_of(Integer)
+  end
+
+  it 'Checks if the direction commands are valid' do
+    i = 0
+    while i < input_data[2][0].length do
+      expect(input_data[2][0][i]).to be_an_instance_of(String)
+      i += 1
+    end
+  end
+
+
+
+
   # it 'A Missing Parameter' do
   #   rover = Mars.new
   # end
