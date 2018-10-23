ffa4dd307ff8153bcdb832886bc95cb7f93b8410
Daniel Valle <daniel.vallecl@gmail.com>
2018-10-23 01:43:07 -0400

    First tests working

--- spec/spec_mars_rover.rb
+++ spec/spec_mars_rover.rb
@@ -3,12 +3,20 @@ require "spec_helper"
 require_relative "../mars_rover"
 require_relative "../class_mars"
 
-describe MarsRover do
+describe "Mars Rover Functions" do
 
-  # it 'Check if there is a Rover' do
-  #   rover = MarsRover.new
-  # end
-  #
+  it 'Checks for the presence of an Input file' do
+    rover = MarsRover.new
+    expect(rover.read_input_file("mars_input.txt").size).to eq(5)
+  end
+
+  it 'Checks if there is a Plateau X and Y Information' do
+    rover = MarsRover.new
+    expect(rover.read_input_file("mars_input.txt")[0]).to eq("5 5\n")
+  end
+
+  
+
   # it 'A direction other than North, East, South or West' do
   #   rover = Mars.new
   # end
@@ -23,6 +31,13 @@ describe MarsRover do
   #
   # it 'A position over the limits of the plateau' do
   #   rover = Mars.new
+  # end
+
+  # it 'Length of String - String Check' do
+  #   emptytext = StrFunction.new
+  #   str = ""
+  #   result = emptytext.len(str)
+  #   expect(result).to eq(0)
   # end
 
 
